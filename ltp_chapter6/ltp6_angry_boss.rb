12153298e19ab002a05ab4c0cc2402184f5eea7f
Nars Dichoso <narscor@gmail.com>
2013-05-13 17:38:51 +0300

    Added new folder 'ltp6_chapter6'

---
+++ ltp_chapter6/ltp6_angry_boss.rb
@@ -0,0 +1,5 @@
+
+
+puts 'Hey, yo Nars, what makes you disturb my nap today, huh!?%$&^%*!'
+reason = gets.chomp.upcase
+puts "WHADDAYA MEAN '#{reason}'??!!?? YOU\'RE FIRED!!!"
---
+++ ltp_chapter6/ltp6_table_of_contents.rb
@@ -0,0 +1,12 @@
+# Learn to Program Book (p. 32), Chapter 6 Exercise
+# Table of Contents
+puts
+title = 'Table of Contents'.center(80)
+chap_1 = 'Chapter 1: Getting Started'.ljust(40)  +  'page 1'.rjust(40)
+chap_2 = 'Chapter 2: Numbers'.ljust(40)  +  'page 2'.rjust(40)
+chap_3 = 'Chapter 3: Letters'.ljust(40)  +  'page 3'.rjust(40)
+puts
+puts title
+puts chap_1
+puts chap_2
+puts chap_3
