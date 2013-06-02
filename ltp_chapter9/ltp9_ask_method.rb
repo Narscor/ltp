8c8cdab6b4d8d06ed14d2ef830d3f297976c64e1
Nars Dichoso <narscor@gmail.com>
2013-06-03 00:28:01 +0300

    Added folder ltp_chapter9/ltp9_ask_method.rb.

---
+++ ltp_chapter9/ltp9_ask_method.rb
@@ -0,0 +1,46 @@
+# Attempted solution to Chris Pine's Learn To Program Book (Page 68) 
+# Chapter 9  Exercise on "Improved ask method"
+puts
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply =='yes'
+      return true
+    end
+
+    if reply == 'no'
+      return false
+    end
+   
+    puts "Please answer 'yes' or 'no'."
+  end
+end
+puts
+puts "Hello, and thank you for taking the time to"
+puts "help me with this experiment. My experiment"
+puts "has to do with the way people feel about"
+puts "Mexican food. Just think about Mexican food"
+puts "and try to answer every question honestly,"
+puts "either a 'yes' or a 'no'. My experiment"
+puts "has nothing to do with bed-wetting."
+puts
+likes = ask 'Do you like eating tacos?'
+likes = ask 'Do you like eating buritos?'
+wets_bed = ask 'Do you wet the bed?'
+likes = ask 'Do you like eating chimichangas?'
+puts 'Just a few more questions . . .'
+likes = ask 'Do you like drinking horchata?'
+likes = ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with'
+puts 'this experiment. In fact, this experiment'
+puts 'has nothing to do with Mexican food. It is'
+puts 'an experiment about bed-wetting. The Mexican'
+puts 'food was just there to catch you off guard'
+puts 'in the hopes that you would answer more'
+puts 'honestly. Thanks again. :-)'
+puts
+puts likes
+puts wets_bed
