1d60de92bfcfadcca3a1ebc4a2010c679b944854
Nars Dichoso <narscor@gmail.com>
2013-06-04 22:53:57 +0300

    Added ltp9_old_roman.rb and ltp9_modern_roman.rb

---
+++ ltp_chapter9/ltp9_modern_roman.rb
@@ -0,0 +1,42 @@
+# Attempted solution to Chris Pines' Learn to Program's Exercises
+# Page 68, Chapter 9, "Modern" Roman Numerals
+
+def modern_roman_numeral number
+  thousands = (number        / 1000)
+  hundreds  = (number % 1000 /  100)
+  tens      = (number %  100 /   10)
+  ones      = (number %   10       )
+
+  roman     = 'M' * thousands
+
+  if hundreds == 9
+    roman = roman + 'CM'
+  elsif hundreds == 4
+    roman = roman + 'CD'
+  else
+    roman = roman + 'D' * (number % 1000 / 500)
+    roman = roman + 'C' * (number %  500 / 100)    
+  end
+
+  if tens == 9
+    roman = roman + 'XC'
+  elsif tens == 4
+    roman = roman + 'XL'
+  else
+    roman = roman + 'L' * (number % 100 / 50)
+    roman = roman + 'X' * (number %  50 / 10)
+  end
+
+  if ones == 9
+    roman = roman + 'IX'
+  elsif ones == 4
+    roman = roman + 'IV'
+  else
+    roman = roman + 'V' * (number % 10 / 5)
+    roman = roman + 'I' * (number %  5 / 1)
+  end
+
+  roman
+end
+
+puts (modern_roman_numeral (2013))
